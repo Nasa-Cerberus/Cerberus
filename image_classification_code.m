ef2a574d6c8c934f84080ec4091ebe252fd03bef
Nasa-Cerberus <91830179+Nasa-Cerberus@users.noreply.github.com>
2021-10-04 11:09:23 +0530

    Delete image_classification_code.m

--- image_classification_code.m
+++
@@ -1,6 +0,0 @@
-I = imread("A tidal wave of plastic.jpg");
-I = imresize(I, [224 224]);
-[YPred,probs] = classify(trainedNetwork_1,I);
-imshow(I)
-label = YPred;
-title(string(label) + ", " + num2str(100*max(probs),3) + "%");
